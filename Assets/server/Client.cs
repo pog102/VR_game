d8b2085ae09bd646bb4c053e4d91bea644e2f991
pog102 <ernestasdamanskis@gmail.com>
2026-01-07 20:04:02 +0200

    Send request through buttons

--- Assets/server/Client.cs
+++ Assets/server/Client.cs
@@ -3,11 +3,17 @@ using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
 
+// [System.Serializable]
+// public class PlayerData
+// {
+//     public string type; // "name"
+//     public string variable; // player's name
+// }
 [System.Serializable]
-public class PlayerData
+public class MessageData
 {
-    public string type; // "name"
-    public string variable; // player's name
+    public string type;
+    public string variable;
 }
 
 public class Client : MonoBehaviour
@@ -15,38 +21,46 @@ public class Client : MonoBehaviour
     [SerializeField]
     public string ServerIP = "192.168.4.1";
     public ushort ServerPort = 7777;
+
+    public string name = "Ernis";
     private TcpClient client;
     private NetworkStream stream;
-    public Button myButton;
 
-    void OnButtonClick()
+    // public Button myButton;
+
+    public void StartGame()
     {
-        Send("Test Send");
+        // Debug.Log("Start Game");
+        Send(new MessageData { type = "start" });
     }
 
-    void SendJson(PlayerData data)
+    public void SelectChoice()
+    {
+        Send(new MessageData { type = "answer", variable = "C" });
+    }
+
+    // void SendJson(PlayerData data) { }
+
+    void Send(MessageData data)
     {
         string json = JsonUtility.ToJson(data);
-        Send(json);
+        Debug.Log(json.ToString());
+        byte[] bytes = Encoding.UTF8.GetBytes(json + "\n");
+        stream.Write(bytes, 0, bytes.Length);
     }
 
     void Start()
     {
-        myButton.onClick.AddListener(OnButtonClick);
+        // myButton.onClick.AddListener(OnButtonClick);
         Connect();
-        SendJson(new PlayerData { type = "name", variable = "Sugoi22" });
+        Send(new MessageData { type = "name", variable = name });
+        // StartGame();
     }
 
     void Connect()
     {
         client = new TcpClient(ServerIP, ServerPort);
         stream = client.GetStream();
-    }
-
-    void Send(string message)
-    {
-        byte[] data = Encoding.UTF8.GetBytes(message);
-        stream.Write(data, 0, data.Length);
     }
 
     void Update()
