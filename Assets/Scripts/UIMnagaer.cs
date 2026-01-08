b98da707dd8b2643f6d8bba876c7359226bdcb0c
pog102 <ernestasdamanskis@gmail.com>
2026-01-08 11:59:37 +0200

    working on errors

--- Assets/Scripts/UIMnagaer.cs
+++ Assets/Scripts/UIMnagaer.cs
@@ -2,29 +2,29 @@ using UnityEngine;
 
 public class UIMnagaer : MonoBehaviour
 {
-   [Header("UI Panels")]
-    public GameObject StartMenu;
-    public GameObject InputMenu;
-     public GameObject Keyboard;
+//    [Header("UI Panels")]
+//     public GameObject StartMenu;
+//     public GameObject InputMenu;
+//      public GameObject Keyboard;
 
-    void Start()
-    {
-        // Initial state
-        ShowMenu();
-    }
-    public ShowStartMenu()
-    {
-         StartMenu.SetActive(true);
-    }
-    public void ShowMenu()
-    {
-        menuUI.SetActive(true);
-        gameUI.SetActive(false);
-    }
+//     void Start()
+//     {
+//         // Initial state
+//         ShowMenu();
+//     }
+//     public ShowStartMenu()
+//     {
+//          StartMenu.SetActive(true);
+//     }
+//     public void ShowMenu()
+//     {
+//         menuUI.SetActive(true);
+//         gameUI.SetActive(false);
+//     }
 
-    public void ShowGame()
-    {
-        menuUI.SetActive(false);
-        gameUI.SetActive(true);
-    }
+//     public void ShowGame()
+//     {
+//         menuUI.SetActive(false);
+//         gameUI.SetActive(true);
+//     }
 }
