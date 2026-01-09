a3c8269764cc1e6c48961386691b3af6d7b5be9f
pog102 <ernestasdamanskis@gmail.com>
2026-01-09 10:17:04 +0200

    Keyboard sounds and fully functional

--- Assets/Scripts/KeyboardManager.cs
+++ Assets/Scripts/KeyboardManager.cs
@@ -5,12 +5,21 @@ public class KeyboardManager : MonoBehaviour
 {
     public TMP_InputField targetInput;
     public Client client;
+    private AudioSource audioSource;
+    public AudioClip  clickSound;
+
+    void Awake()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+    }
     public void AddChar(string c)
     {
         if (!targetInput) return;
 
         targetInput.text += c;
         targetInput.caretPosition = targetInput.text.Length;
+         audioSource.PlayOneShot(clickSound);
     }
 
     public void Backspace()
@@ -22,11 +31,13 @@ public class KeyboardManager : MonoBehaviour
             targetInput.text.Substring(0, targetInput.text.Length - 1);
 
         targetInput.caretPosition = targetInput.text.Length;
+                 audioSource.PlayOneShot(clickSound);
     }
 
 
     public void Submit()
     {
+        
         client.Send("name", targetInput.text);
         SceneManager.LoadScene("Game");
         // client.Send("name", "BOB");
