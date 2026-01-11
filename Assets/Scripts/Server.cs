4f8b32ed1a072a4a11eda1049f5edaaf1d274c08
pog102 <ernestasdamanskis@gmail.com>
2026-01-11 12:56:40 +0200

    downsize the code

--- Assets/Scripts/Server.cs
+++ Assets/Scripts/Server.cs
@@ -4,7 +4,6 @@ using System.Collections;
 
 public class Server : NetworkBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
     string[] args = System.Environment.GetCommandLineArgs(); 
@@ -17,20 +16,9 @@ public class Server : NetworkBehaviour
                 NetworkManager.Singleton.StartServer();
                 // Serverines Logs
                 NetworkManager.Singleton.OnClientConnectedCallback += ClientConnectMessage;        
-                // StartCoroutine(PeriodicLog());
                 SendClientRpc("Just a send");
-                // serverOff=false;
             }
         }
-           // if (serverOff)  
-           //  { 
-           //      Debug.Log("Running Client.");
-           //      NetworkManager.Singleton.StartClient();
-           //      // Serverines Logs
-           //      // NetworkManager.Singleton.OnClientConnectedCallback += ClientConnectMessage;        
-           //      // StartCoroutine(PeriodicLog());
-           //      // SendClientRpc("Just a send");
-           //  }
 
     }
     public void ClientConnectMessage(ulong connectionId)
@@ -38,21 +26,10 @@ public class Server : NetworkBehaviour
         // Serverines Logs
         Debug.Log("------ client " + connectionId + " connected.");
     }
-    public IEnumerator PeriodicLog()
-    {
-        string msg = "Holla";
-        yield return new WaitForSeconds(10f);
-        while (true)
-        {
-        yield return new WaitForSeconds(10f);
-            Debug.Log("Sending"+msg);
-            SendClientRpc(msg);
-        }
-    }
     [ClientRpc]
     public void SendClientRpc(string message)
     {
-        // Kliento Logs
+        // Kliento Logs (siunciamas klientui is serverio)
         Debug.Log("Message from server: " + message);
     }
 }
