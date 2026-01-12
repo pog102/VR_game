922b31cc8f72875e94e4e744bcb0fc2d7cc1b645
pog102 <ernestasdamanskis@gmail.com>
2026-01-12 11:09:09 +0200

    game.apk

--- SendFile.bat
+++ SendFile.bat
@@ -1,10 +1,4 @@
 @echo off
 
-set USER=vruser
-set HOST=172.16.19.69
-set REMOTE_PATH=/home/vruser/
-set LOCAL_FILE=.\server.x86_64
-
-scp "%LOCAL_FILE%" %USER%@%HOST%:%REMOTE_PATH%
-
+scp .\server.x86_64 vruser@172.16.19.167:/home/vruser/
 
