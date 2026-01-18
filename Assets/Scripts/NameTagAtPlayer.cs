4bc6b95f98c05a141cfd93a567d56a877a4e95b8
pog102 <ernestasdamanskis@gmail.com>
2026-01-18 16:26:45 +0200

    PlayerTags + faces the camera

---
+++ Assets/Scripts/NameTagAtPlayer.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class NameTagAtPlayer : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Transform cam;
+
+    void Start()
+    {
+        cam = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        transform.LookAt(cam);
+        transform.Rotate(0, 180, 0);
+    }
+    // void Update()
+    // {
+    //     transform.LookAt(cam);
+    // }
+}
--- Assets/Scripts/NetwrokPlayer.cs
+++ Assets/Scripts/NetwrokPlayer.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 public class NetwrokPlayer : NetworkBehaviour
 {
+    public GameObject nameTag;
+
+    // public Transform nameTag;
     public Transform head;
     public Transform leftHand;
     public Transform rightHand;
@@ -10,11 +13,15 @@ public class NetwrokPlayer : NetworkBehaviour
 
     public Renderer[] meshToDisable;
 
+    // private Transform localCameraTransform;
+
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
+        // localCameraTransform = Camera.main.transform;
         if (IsOwner)
         {
+            // nameTag.enabled = false;
             foreach (var item in meshToDisable)
             {
                 item.enabled = false;
@@ -24,7 +31,6 @@ public class NetwrokPlayer : NetworkBehaviour
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-    // Update is called once per frame
     void Update()
     {
         if (IsOwner)
@@ -35,11 +41,26 @@ public class NetwrokPlayer : NetworkBehaviour
             head.position = VrRigReference.Singleton.head.position;
             head.rotation = VrRigReference.Singleton.head.rotation;
 
+            // nameTag.position = VrRigReference.Singleton.head.position;
+            // nameTag.rotation = VrRigReference.Singleton.head.rotation;
+            // nameTag.transform.position = VrRigReference.Singleton.root.position;
+            nameTag.transform.position = head.position + Vector3.up * 0.25f;
+            // nameTag.transform.LookAt(cam);
+            // nameTag.transform.rotation = VrRigReference.Singleton.root.rotation;
+            // nameTag.transform.Rotate(trans);
             leftHand.position = VrRigReference.Singleton.leftHand.position;
             leftHand.rotation = VrRigReference.Singleton.leftHand.rotation;
 
             rightHand.position = VrRigReference.Singleton.rightHand.position;
             rightHand.rotation = VrRigReference.Singleton.rightHand.rotation;
         }
+        // if (Camera.main != null)
+        // {
+        //     // Make the nametag look at the local player's camera
+        //     nameTag.transform.LookAt(localCameraTransform);
+        //
+        //     // Optional: Flip it 180 degrees if the text appears backward
+        //     // nameTag.transform.Rotate(0, 180, 0);
+        // }
     }
 }
